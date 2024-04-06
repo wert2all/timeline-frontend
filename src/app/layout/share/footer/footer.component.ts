395ff6ebb531515d4cd1bd9daf4bb7661e38f287
Serhii Shvets <wert2all@gmail.com>
2024-04-06 19:39:12 +0300

    feat: add youtube icon and link

--- src/app/layout/share/footer/footer.component.ts
+++ src/app/layout/share/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { saxVideoSquareOutline } from '@ng-icons/iconsax/outline';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIconComponent],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ saxVideoSquareOutline })],
 })
 export class FooterComponent {}
