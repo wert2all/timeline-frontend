881c89878947916377ba6272ce6f0d9ec68e048d
Serhii <wert2all@gmail.com>
2024-04-06 20:48:31 +0300

    feat: add store for notifications

---
+++ src/app/store/notifications/notifications.store.ts
@@ -0,0 +1,48 @@
+import { computed } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { v4 as uuid } from 'uuid';
+
+import { MessageType, NotificationsState } from './notifications.types';
+
+const initialState: NotificationsState = { messages: [] };
+export const NotificationStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withComputed(state => ({
+    unRead: computed(() => state.messages().filter(message => !message.read)),
+  })),
+  withMethods(store => ({
+    markRead(uuid: string): void {
+      patchState(store, state => ({
+        ...state,
+        messages: state.messages.map(message => ({
+          ...message,
+          read: message.uuid === uuid,
+        })),
+      }));
+    },
+    addMessage(message: string, type: MessageType): void {
+      patchState(
+        store,
+        (state): NotificationsState => ({
+          ...state,
+          messages: [
+            ...state.messages,
+            {
+              uuid: uuid(),
+              type: type,
+              read: false,
+              message: message,
+            },
+          ],
+        })
+      );
+    },
+  }))
+);
