a2c74388d4bb6385180420a8b285e64d48ca87ab
Serhii Shvets <wert2all@gmail.com>
2024-04-06 19:42:33 +0300

    feat: add iconto login button

--- src/app/pages/login/login-page.component.ts
+++ src/app/pages/login/login-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { NoAuthorizedLayoutComponent } from '../../layout/no-authorized/no-authorized-layout.component';
-
+import { saxLogin1Outline } from '@ng-icons/iconsax/outline';
 @Component({
   selector: 'app-login-page',
   standalone: true,
   templateUrl: './login-page.component.html',
-  imports: [NoAuthorizedLayoutComponent],
+  imports: [NoAuthorizedLayoutComponent, NgIconComponent],
+  viewProviders: [provideIcons({ saxLogin1Outline })],
 })
 export class LoginPageComponent {}
