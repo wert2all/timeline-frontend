cb51fe627020591db248631a1f2c8a1e269bf2c6
Serhii Shvets <wert2all@gmail.com>
2024-04-07 00:14:42 +0300

    feat: add time line to index  component

--- src/app/pages/non-authorized/index/index-page.component.ts
+++ src/app/pages/non-authorized/index/index-page.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroCheckCircleSolid } from '@ng-icons/heroicons/solid';
 import { NoAuthorizedLayoutComponent } from '../../../layout/no-authorized/no-authorized-layout.component';
-
 @Component({
   selector: 'app-index-page',
   standalone: true,
-  imports: [CommonModule, NoAuthorizedLayoutComponent],
+  imports: [CommonModule, NoAuthorizedLayoutComponent, NgIconComponent],
   templateUrl: './index-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ heroCheckCircleSolid })],
 })
 export class IndexPageComponent {}
