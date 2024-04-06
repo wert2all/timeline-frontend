9b1ae2fccb1fbceee9da97154bf456fc10bdc026
Serhii <wert2all@gmail.com>
2024-04-06 17:32:38 +0300

    feat: add gruvbox theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,20 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     logs: false,
+    themes: [
+      {
+        gruvbox: {
+          primary: '#448588',
+          secondary: '#0077d8', // fixme
+          accent: '#0033ff', //fixme
+          neutral: '#1d2021',
+          'base-100': '#282828',
+          info: '#ebdbb2',
+          success: '#98971a',
+          warning: '#d65d0e',
+          error: '#ff2a4d',
+        },
+      },
+    ],
   },
 };
