96a1679c59279c6168243170d5ab25da6ce2e57b
Serhii Shvets <wert2all@gmail.com>
2024-04-06 23:29:30 +0300

    feat: add index component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { IndexPageComponent } from './pages/non-authorized/index/index-page.component';
 import { LoginPageComponent } from './pages/non-authorized/login/login-page.component';
 
 export const routes: Routes = [
+  { path: '', component: IndexPageComponent },
   {
-    path: '',
+    path: 'user/login',
     component: LoginPageComponent,
   },
 ];
