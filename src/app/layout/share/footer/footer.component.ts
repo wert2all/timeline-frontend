d9fccbc97eacfde13009f26be065c0f4382cb9ae
Serhii Shvets <wert2all@gmail.com>
2024-04-06 19:32:03 +0300

    feat: add footer

---
+++ src/app/layout/share/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {}
