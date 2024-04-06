e4b69ae66549975a73040235325f0789bc27329e
Serhii Shvets <wert2all@gmail.com>
2024-04-06 23:20:19 +0300

    refactor: move pages to non-authorized namespace

--- src/app/pages/non-authorized/login/login-page.component.ts
+++ src/app/pages/non-authorized/login/login-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 
-import { NoAuthorizedLayoutComponent } from '../../layout/no-authorized/no-authorized-layout.component';
+import { NoAuthorizedLayoutComponent } from '../../../layout/no-authorized/no-authorized-layout.component';
 import { LoginContainerComponent } from './widgets/login/login-container.component';
 @Component({
   selector: 'app-login-page',
