751056dbe6e54753f10adbeafcbb2a284ca21350
Serhii Shvets <wert2all@gmail.com>
2024-04-06 19:32:03 +0300

    feat: add footer

--- src/app/layout/no-authorized/no-authorized-layout.component.ts
+++ src/app/layout/no-authorized/no-authorized-layout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../share/footer/footer.component';
 
 @Component({
   selector: 'app-no-authorized-layout',
   standalone: true,
   templateUrl: './no-authorized-layout.component.html',
+  imports: [FooterComponent],
 })
 export class NoAuthorizedLayoutComponent {}
