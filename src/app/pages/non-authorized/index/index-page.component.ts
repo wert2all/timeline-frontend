33398bb95e201d81459752154966fbead4b9848e
Serhii Shvets <wert2all@gmail.com>
2024-04-07 13:28:53 +0300

    feat: wip: add tags

--- src/app/pages/non-authorized/index/index-page.component.ts
+++ src/app/pages/non-authorized/index/index-page.component.ts
@@ -21,10 +21,12 @@ enum TimelimeEventType {
 }
 
 type TimelineRequired = { date: Date; type: TimelimeEventType };
+
 type TimelineEvent = TimelineRequired & {
   title?: string;
   description?: string;
   url?: string;
+  tags?: string[];
 };
 
 class ViewTimelineEventIcon {
@@ -40,6 +42,13 @@ class ViewTimelineEventIcon {
   }
 }
 
+class ViewTimelineTag {
+  public readonly title: string;
+  constructor(title: string) {
+    this.title = '#' + title;
+  }
+}
+
 type ViewTimelineDate = {
   raw: Date;
   withTime: string;
@@ -47,10 +56,11 @@ type ViewTimelineDate = {
 };
 
 type ViewTimelineUrl = { title: string; link: string };
-type ViewTimelineEvent = Omit<TimelineEvent, 'date' | 'url'> & {
+type ViewTimelineEvent = Omit<TimelineEvent, 'date' | 'url' | 'tags'> & {
   icon: ViewTimelineEventIcon;
   date: ViewTimelineDate;
-  url?: ViewTimelineUrl | null;
+  url: ViewTimelineUrl | null;
+  tags: ViewTimelineTag[];
   changeDirection: boolean;
 };
 @Component({
@@ -72,6 +82,7 @@ export class IndexPageComponent {
       date: new Date('2024-04-06T03:29:00.000+03:00'),
       type: TimelimeEventType.selebrate,
       title: 'first commit to timelime was pushed',
+      tags: ['party', 'selebrate', 'start', 'timeline', 'important'],
       description:
         "We made our first commit to our new project. \n The first commit was a small one, but it represents a big step forward for us. It is a testament to the hard work and dedication of our team, who have been working tirelessly to make this project a reality. \n We are excited to continue working on 'timeline' and to share it with the world in the future. We believe that it has the potential to make a real difference in people's lives, and we are committed to making that happen.",
       url: 'https://github.com/wert2all/timeline-frontend/commit/613b8c200ab167c594965751c6c1e6ee6c873dad',
@@ -96,11 +107,15 @@ export class IndexPageComponent {
         url: this.prepareUrl(event.url),
         date: date,
         changeDirection: index % 2 === 0,
+        tags: this.createTags(event.tags),
       };
     });
   });
 
   private prepareUrl(url: string | undefined) {
     return url ? { title: 'Read more', link: url } : null;
+  }
+  private createTags(tags: string[] | undefined) {
+    return tags?.map(tag => new ViewTimelineTag(tag)) || [];
   }
 }
