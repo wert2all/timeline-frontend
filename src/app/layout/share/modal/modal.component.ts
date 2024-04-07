2eaf1c39b78f628128ffe90f7a09c704e3ca63a8
Serhii <wert2all@gmail.com>
2024-04-07 20:45:19 +0300

    feat: add modal dialog for adding event component

---
+++ src/app/layout/share/modal/modal.component.ts
@@ -0,0 +1,30 @@
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  effect,
+  input,
+  output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  standalone: true,
+  templateUrl: './modal.component.html',
+})
+export class ModalComponent {
+  @ViewChild('modal') private modal: ElementRef<HTMLDialogElement> | null =
+    null;
+
+  show = input.required<number>();
+  onClosed = output<boolean>();
+
+  constructor() {
+    effect(() => {
+      if (this.show() > 0) {
+        this.modal?.nativeElement.showModal();
+      }
+    });
+  }
+}
