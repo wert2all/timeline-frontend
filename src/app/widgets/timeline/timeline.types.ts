ee9f8a36c9594cfb8f718fb64981efbe2d9b4be6
wert2all <wert2all@gmail.com>
2024-04-16 15:28:11 +0300

    feat: add preview if change values of add form

--- src/app/widgets/timeline/timeline.types.ts
+++ src/app/widgets/timeline/timeline.types.ts
@@ -21,9 +21,14 @@ export type TimelineEventDraft = TimelineEvent & {
 };
 
 export class ViewTimelineTag {
-  public readonly title: string;
-  constructor(title: string) {
-    this.title = '#' + title;
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  public get title(): string {
+    return '#' + this.value;
   }
 }
 export type ViewTimelineUrl = { title: string; link: string };
@@ -59,3 +64,13 @@ export type ViewTimelineEvent = Omit<TimelineEvent, 'date' | 'url' | 'tags'> & {
 export type ViewTimelineEventDraft = ViewTimelineEvent & {
   draft: boolean;
 };
+
+export type AddValue = Partial<{
+  date: string | null;
+  time: string | null;
+  withTime: boolean | null;
+  showTime: boolean | null;
+  title: string | null;
+  content: string | null;
+  tags: string[];
+}>;
