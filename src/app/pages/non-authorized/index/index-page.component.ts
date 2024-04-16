eb77cfadc356637851ff881a4873c199e6068a34
wert2all <wert2all@gmail.com>
2024-04-16 14:37:04 +0300

    refactor: move add event forn into timeline component

--- src/app/pages/non-authorized/index/index-page.component.ts
+++ src/app/pages/non-authorized/index/index-page.component.ts
@@ -1,31 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroCheckCircleSolid } from '@ng-icons/heroicons/solid';
-import { NoAuthorizedLayoutComponent } from '../../../layout/no-authorized/no-authorized-layout.component';
 
-import { saxLinkOutline } from '@ng-icons/iconsax/outline';
-import { AddEventComponent } from '../../../components/add-event/add-event.component';
-import { DateComponent } from '../../../widgets/timeline/event/date/date.component';
-import { TagsComponent } from '../../../widgets/timeline/event/tags/tags.component';
-import { UrlComponent } from '../../../widgets/timeline/event/url/url.component';
 import { TimelineComponent } from '../../../widgets/timeline/timeline.component';
+import { NoAuthorizedLayoutComponent } from '../../../layout/no-authorized/no-authorized-layout.component';
 
 @Component({
   selector: 'app-index-page',
   standalone: true,
   templateUrl: './index-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  viewProviders: [provideIcons({ heroCheckCircleSolid, saxLinkOutline })],
-  imports: [
-    CommonModule,
-    NoAuthorizedLayoutComponent,
-    NgIconComponent,
-    TagsComponent,
-    DateComponent,
-    UrlComponent,
-    TimelineComponent,
-    AddEventComponent,
-  ],
+  imports: [CommonModule, TimelineComponent, NoAuthorizedLayoutComponent],
 })
 export class IndexPageComponent {}
