56fcd8f45e1f17adef9e7de4c14dad8e96f55cab
Serhii Shvets <wert2all@gmail.com>
2024-04-17 21:51:36 +0300

    feat: add apollo client

---
+++ src/app/api/graphql.ts
@@ -0,0 +1,44 @@
+import { gql } from 'apollo-angular';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never;
+    };
+/** All built-in and custom scalars, mapped to their actual values */
+export interface Scalars {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+}
+
+export type User = { id: number; name: string; email: string };
+
+export const User = gql`
+  fragment User on User {
+    id
+    name
+    email
+  }
+`;
+
+@Injectable({ providedIn: 'root' })
+export class ApiClient {
+  constructor() {}
+}
