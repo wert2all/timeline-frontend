9c453efd55d9c2848571c5ecbd68a9d9b78de259
Serhii Shvets <wert2all@gmail.com>
2024-04-20 20:02:04 +0300

    fix: redirect to home page on login

--- src/app/pages/non-authorized/login/login-page.component.ts
+++ src/app/pages/non-authorized/login/login-page.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 
+import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { filter } from 'rxjs';
 import { NoAuthorizedLayoutComponent } from '../../../layout/no-authorized/no-authorized-layout.component';
+import { authFeature } from '../../../store/auth/auth.reducer';
 import { LoginContainerComponent } from './widgets/login/login-container.component';
 @Component({
   selector: 'app-login-page',
@@ -11,6 +17,20 @@ import { LoginContainerComponent } from './widgets/login/login-container.compone
     NoAuthorizedLayoutComponent,
     NgIconComponent,
     LoginContainerComponent,
+    AsyncPipe,
   ],
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  isAuthorised = inject(Store).select(authFeature.isAuthorized);
+
+  constructor(router: Router) {
+    this.isAuthorised
+      .pipe(
+        takeUntilDestroyed(),
+        filter(isAuth => isAuth)
+      )
+      .subscribe(() => {
+        router.navigate(['/']);
+      });
+  }
+}
