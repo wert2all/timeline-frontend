edfe4d95164d3121303c2372815cb83de894aa7b
wert2all <wert2all@gmail.com>
2024-04-25 11:40:49 +0300

    refactor: refactor login process to state

---
+++ src/app/api/external/google-outh.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { environment } from '../../../environments/environment';
+import {
+  CredentialResponse,
+  GoogleUserInfo,
+  IdConfiguration,
+} from '../../store/auth/auth.types';
+
+export interface Listeners {
+  onSignIn: (token: string, user: GoogleUserInfo) => void;
+  onNotDisplayed: (reason: string) => void;
+  onNotVerifiedEmail: () => void;
+}
+const defaultListeners: Listeners = {
+  onSignIn: () => {},
+  onNotDisplayed: () => {},
+  onNotVerifiedEmail: () => {},
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GoogleOuthService {
+  private isInitialized: boolean = false;
+  private readonly clientId = environment.googleClientId;
+  private googleConfiguration: IdConfiguration = {
+    client_id: this.clientId,
+    callback: this.getResponseCallback.bind(this),
+    auto_select: true,
+    cancel_on_tap_outside: false,
+  };
+
+  private listeners: Listeners = defaultListeners;
+
+  login(listeners: Listeners) {
+    this.initialize(listeners);
+    google.accounts.id.prompt(prompt => {
+      if (prompt.isNotDisplayed()) {
+        this.listeners.onNotDisplayed(prompt.getNotDisplayedReason());
+      }
+    });
+  }
+
+  private initialize(listeners: Listeners) {
+    if (!this.isInitialized) {
+      google.accounts.id.initialize(this.googleConfiguration);
+      this.isInitialized = true;
+    }
+    this.listeners = listeners;
+  }
+
+  private getResponseCallback(response: CredentialResponse) {
+    const userInfo = jwtDecode(response.credential) as GoogleUserInfo;
+    if (userInfo.email_verified) {
+      this.listeners.onSignIn(response.credential, userInfo);
+    } else {
+      this.listeners.onNotVerifiedEmail();
+    }
+  }
+}
