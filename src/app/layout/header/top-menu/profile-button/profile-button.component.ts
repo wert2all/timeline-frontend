cd6e2a822564912a9ccf0565bac60cca76933582
wert2all <wert2all@gmail.com>
2024-04-26 10:47:13 +0300

    feat: add profile button

---
+++ src/app/layout/header/top-menu/profile-button/profile-button.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+
+@Component({
+  selector: 'app-profile-button',
+  standalone: true,
+  imports: [CommonModule, NgIconComponent],
+  templateUrl: './profile-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({})],
+})
+export class ProfileButtonComponent {
+  isLoading = input.required<boolean>();
+  user = input.required<{ avatar: string; name: string } | null>();
+
+  toProfile = output();
+}
--- src/app/layout/header/top-menu/top-menu.component.ts
+++ src/app/layout/header/top-menu/top-menu.component.ts
@@ -5,19 +5,28 @@ import { AuthActions } from '../../../store/auth/auth.actions';
 import { authFeature } from '../../../store/auth/auth.reducer';
 import { AuthControlButtonComponent } from './auth-control-button/auth-control-button.component';
 import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import { ProfileButtonComponent } from './profile-button/profile-button.component';
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
   templateUrl: './top-menu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, AuthControlButtonComponent, LogoutButtonComponent],
+  imports: [
+    CommonModule,
+    AuthControlButtonComponent,
+    LogoutButtonComponent,
+    ProfileButtonComponent,
+  ],
 })
 export class TopMenuComponent {
   private readonly store = inject(Store);
 
   isLoading = this.store.selectSignal(authFeature.isLoading);
+  // isLoading = signal(true);
   isAuthorized = this.store.selectSignal(authFeature.isAuthorized);
+
+  authorizedUser = this.store.selectSignal(authFeature.selectAuthorizedUser);
 
   logout() {
     this.store.dispatch(AuthActions.logout());
