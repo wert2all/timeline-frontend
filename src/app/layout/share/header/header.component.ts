475561cd5a309cba73892241eeb176e1a6677ff7
wert2all <wert2all@gmail.com>
2024-04-26 09:34:39 +0300

    refactor: move children of heafr components into header namespace

--- src/app/layout/share/header/header.component.ts
+++ src/app/layout/share/header/header.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { saxMenu1Outline } from '@ng-icons/iconsax/outline';
-import { TopMenuComponent } from '../top-menu/top-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
