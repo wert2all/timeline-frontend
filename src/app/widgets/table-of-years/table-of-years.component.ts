56ac94bc7e925afe6110db7ef6742fbf9c62806d
Serhii Shvets <wert2all@gmail.com>
2024-09-17 23:51:19 +0300

    feat: add table of years component

---
+++ src/app/widgets/table-of-years/table-of-years.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-of-years',
+  standalone: true,
+  templateUrl: './table-of-years.component.html',
+})
+export class TableOfYearsComponent { }
