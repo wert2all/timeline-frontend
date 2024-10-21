12910958b15153cf5be329ea44d76cd491b3aec0
Serhii Shvets <wert2all@gmail.com>
2024-10-21 22:31:35 +0300

    feat: add previewly GQL codegen

--- codegen.ts
+++ codegen.ts
@@ -1,10 +1,50 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 import { environment } from './src/environments/environment';
 
+const operationsConfig = (serviceName: string) => ({
+  addExplicitOverride: true,
+  omitOperationSuffix: true,
+  querySuffix: 'Query',
+  mutationSuffix: 'Mutation',
+  serviceName: serviceName,
+  sdkClass: true,
+  skipTypename: true,
+
+  skipTypeNameForRoot: true,
+  preResolveTypes: true,
+  declarationKind: 'interface',
+  onlyOperationTypes: true,
+  inlineFragmentTypes: 'combine',
+
+  namingConvention: {
+    enumValues: 'keep',
+  },
+  strictScalars: true,
+  scalars: {
+    Time: 'string',
+  },
+});
+
 const config: CodegenConfig = {
   overwrite: true,
   emitLegacyCommonJSImports: false,
   generates: {
+    './src/app/api/external/previewly/schema.graphql': {
+      schema: 'https://api.previewly.top/graphql',
+      documents: ['./src/app/api/external/previewly/**/*.graphql'],
+      plugins: ['schema-ast'],
+    },
+
+    './src/app/api/external/previewly/graphql.ts': {
+      schema: 'https://api.previewly.top/graphql',
+      documents: ['./src/app/api/external/previewly/**/*.graphql'],
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-apollo-angular',
+      ],
+      config: operationsConfig('PreviewlyApiClient'),
+    },
     './src/app/api/internal/schema.graphql': {
       schema: environment.graphql,
       documents: ['./src/app/api/internal/**/*.graphql'],
@@ -18,29 +58,7 @@ const config: CodegenConfig = {
         'typescript-operations',
         'typescript-apollo-angular',
       ],
-      config: {
-        addExplicitOverride: true,
-        omitOperationSuffix: true,
-        querySuffix: 'Query',
-        mutationSuffix: 'Mutation',
-        serviceName: 'ApiClient',
-        sdkClass: true,
-        skipTypename: true,
-
-        skipTypeNameForRoot: true,
-        preResolveTypes: true,
-        declarationKind: 'interface',
-        onlyOperationTypes: true,
-        inlineFragmentTypes: 'combine',
-
-        namingConvention: {
-          enumValues: 'keep',
-        },
-        strictScalars: true,
-        scalars: {
-          Time: 'string',
-        },
-      },
+      config: operationsConfig('ApiClient'),
     },
   },
   hooks: { afterAllFileWrite: ['prettier --write'] },
---
+++ src/app/api/external/previewly/graphql.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { gql } from 'apollo-angular';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type MakeEmpty<
+  T extends { [key: string]: unknown },
+  K extends keyof T,
+> = { [_ in K]?: never };
+export type Incremental<T> =
+  | T
+  | {
+      [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never;
+    };
+/** All built-in and custom scalars, mapped to their actual values */
+export interface Scalars {
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+}
+
+export enum Status {
+  error = 'error',
+  pending = 'pending',
+  success = 'success',
+}
+
+export type Preview = { id: number };
+
+export const Preview = gql`
+  fragment Preview on PreviewData {
+    id
+  }
+`;
+
+@Injectable({ providedIn: 'root' })
+export class PreviewlyApiClient {
+  constructor() {}
+}
