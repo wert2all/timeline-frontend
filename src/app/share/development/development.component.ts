bcbdeb7c79c1106c77006bb0a7a726aaa73f15c9
wert2all <wert2all@gmail.com>
2024-10-22 15:33:44 +0300

    fix: remove link preview conponent from development component

--- src/app/share/development/development.component.ts
+++ src/app/share/development/development.component.ts
@@ -1,31 +1,16 @@
 import { CommonModule } from '@angular/common';
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-} from '@angular/core';
-import { Store } from '@ngrx/store';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { EnvironmentType } from '../../../environments/environment.types';
-import { PreviewActions } from '../../store/preview/preview.actions';
-import { LinkPreviewComponent } from '../../widgets/link-preview/link-preview.component';
 
 @Component({
   selector: 'app-development',
   standalone: true,
-  imports: [CommonModule, LinkPreviewComponent],
+  imports: [CommonModule],
   templateUrl: './development.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DevelopmentComponent implements AfterViewInit {
-  readonly url = new URL('https://thum.io');
-  private store = inject(Store);
-
-  ngAfterViewInit(): void {
-    this.store.dispatch(PreviewActions.addURL({ url: this.url }));
-  }
-
+export class DevelopmentComponent {
   shouldShow() {
     return environment.type === EnvironmentType.development;
   }
