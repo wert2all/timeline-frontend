2a22bff9ae094546caa27848ae00277342d7254e
wert2all <wert2all@gmail.com>
2024-10-23 14:51:01 +0300

    test(ui): add event form to develop page

--- src/app/pages/non-authorized/develop/develop-page.component.ts
+++ src/app/pages/non-authorized/develop/develop-page.component.ts
@@ -1,11 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Status } from '../../../app.types';
-import { dumpLink } from '../../../share/dump.types';
+import {
+  dumpEvent,
+  dumpLink,
+  dumpLinkTitle,
+  dumpTags,
+} from '../../../share/dump.types';
 import { TitleComponent } from '../../../share/layout/content/title/title.component';
 import { LayoutComponent } from '../../../share/layout/layout.component';
+import { EventAdditionalContentComponent } from '../../../share/timeline/timeline/event/content/additional/additional-content.component';
 import { PreviewHolder } from '../../../store/preview/preview.types';
+import { EditEventFormComponent } from '../../../widgets/timeline-container/edit-event-form/edit-event-form.component';
 import { LinkPreviewComponent } from '../../../widgets/timeline-container/edit-event-form/link-preview/link-preview.component';
+import {
+  EditableTimelineTypes,
+  EditableViewTimelineEvent,
+} from '../../../widgets/timeline-container/timeline.types';
 
 @Component({
   selector: 'app-develop-page',
@@ -15,6 +26,8 @@ import { LinkPreviewComponent } from '../../../widgets/timeline-container/edit-e
     LayoutComponent,
     TitleComponent,
     LinkPreviewComponent,
+    EditEventFormComponent,
+    EventAdditionalContentComponent,
   ],
   templateUrl: './develop-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -44,5 +57,28 @@ export class DevelopPageComponent {
         title: 'Thum.io | Fast real-time website screenshot API',
       },
     },
+  });
+
+  dumpAddEvent = signal<
+    EditableViewTimelineEvent & {
+      eventLength: string;
+      shouldAccentLine: boolean;
+    }
+  >({
+    ...dumpEvent,
+    url: {
+      title: dumpLinkTitle,
+      link: dumpLink,
+    },
+    date: {
+      date: '2024-05-26T15:21:00.000+03:00',
+      relative: '2 days ago',
+    },
+    changeDirection: false,
+    tags: dumpTags,
+    description: 'Some description',
+    type: EditableTimelineTypes.draft,
+    eventLength: 'mb-8',
+    shouldAccentLine: true,
   });
 }
--- src/app/share/dump.types.ts
+++ src/app/share/dump.types.ts
@@ -1,1 +1,19 @@
+import {
+  TimelimeEventType,
+  TimelineEvent,
+} from '../store/timeline/timeline.types';
+import { ViewTimelineTag } from '../widgets/timeline-container/timeline.types';
+
 export const dumpLink = 'https://www.thum.io/';
+export const dumpLinkTitle = 'Link title';
+
+export const dumpEvent: TimelineEvent = {
+  id: 0,
+  date: new Date('2022-01-01 00:00:00'),
+  type: TimelimeEventType.youtube,
+  loading: false,
+};
+
+export const dumpTags: ViewTimelineTag[] = [
+  { value: 'dumpTag', title: '#dumpTag' },
+];
