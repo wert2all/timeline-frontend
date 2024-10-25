8bc830cad137d9f751d1886ed9f935499bdc3152
Serhii Shvets <wert2all@gmail.com>
2024-10-25 16:43:23 +0300

    refactor: refactor to remark module

--- src/app/share/dump.types.ts
+++ src/app/share/dump.types.ts
@@ -5,11 +5,10 @@ import {
 import { ViewTimelineTag } from '../widgets/timeline-container/timeline.types';
 
 export const dumpTitle = 'Dump title';
-export const dumpContent = 'Dump content';
+export const dumpContent = '# Hello, Neptune!';
 export const dumpLink = 'https://www.thum.io/';
 export const dumpLinkTitle = 'Link title';
 export const dumpDate = new Date('2022-10-21 13:34:40');
-
 export const dumpEvent: TimelineEvent = {
   id: 0,
   date: dumpDate,
