dcb20b7e37b5c8829cddfb62fce26add806ee515
wert2all <wert2all@gmail.com>
2024-10-30 13:14:28 +0200

    refactor: refactor timeline container

--- src/app/pages/authorized/my/my-page.component.ts
+++ src/app/pages/authorized/my/my-page.component.ts
@@ -9,9 +9,14 @@ import { Store } from '@ngrx/store';
 import { LayoutComponent } from '../../../share/layout/layout.component';
 
 import { authFeature } from '../../../store/auth/auth.reducer';
-import { TimelineActions } from '../../../store/timeline/timeline.actions';
+import {
+  EventActions,
+  TimelineActions,
+} from '../../../store/timeline/timeline.actions';
 import { timelineFeature } from '../../../store/timeline/timeline.reducer';
 
+import { EditEventComponent } from '../../../feature/edit-event/edit-event.component';
+import { AddEventButtonComponent } from '../../../feature/timeline/components/add-event-button/add-event-button.component';
 import { CreateTimelineButtonComponent } from '../../../feature/timeline/components/create-timeline-button/create-timeline-button.component';
 import { TimelineContainerComponent } from '../../../feature/timeline/timeline-container.component';
 
@@ -25,10 +30,17 @@ import { TimelineContainerComponent } from '../../../feature/timeline/timeline-c
     LayoutComponent,
     CreateTimelineButtonComponent,
     TimelineContainerComponent,
+    EditEventComponent,
+    AddEventButtonComponent,
   ],
 })
 export class MyPageComponent {
   private readonly store = inject(Store);
+
+  private readonly isEditingEvent = this.store.selectSignal(
+    timelineFeature.isEditingEvent
+  );
+  private readonly timelineId = computed(() => this.activeTimeline()?.id || 0);
 
   readonly isAuthLoading = this.store.selectSignal(authFeature.isLoading);
   readonly isTimelineLoading = this.store.selectSignal(
@@ -38,11 +50,21 @@ export class MyPageComponent {
   readonly activeTimeline = this.store.selectSignal(
     timelineFeature.selectActiveTimeline
   );
+  readonly showTipForAddEvent = this.store.selectSignal(
+    timelineFeature.selectNewTimelineAdded
+  );
+  readonly canAddNewEvent = computed(() => !this.isEditingEvent());
 
   isLoading = computed(() => {
     return this.isAuthLoading() || this.isTimelineLoading();
   });
+
   addTimeline(name: string | null) {
     this.store.dispatch(TimelineActions.addTimeline({ name: name }));
+  }
+  dispatchNewEventCreation() {
+    this.store.dispatch(
+      EventActions.showAddEventForm({ timelineId: this.timelineId() })
+    );
   }
 }
