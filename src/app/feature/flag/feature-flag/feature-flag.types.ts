dcd5e4d99c9e2c7b668d9f4219673a3d1af9a3c2
Serhii Shvets <wert2all@gmail.com>
2024-11-02 15:34:49 +0200

    feat: create dropdown user menu on header

--- src/app/feature/flag/feature-flag/feature-flag.types.ts
+++ src/app/feature/flag/feature-flag/feature-flag.types.ts
@@ -2,5 +2,7 @@ export type FeatureFlagName = keyof typeof FEATURE_FLAGS;
 
 export const FEATURE_FLAGS = {
   edit_event: false,
-  profile_dropdown: true,
+  profile_dropdown: false,
+  user_settings: true,
+  user_feature_flag: true,
 } as const satisfies Record<string, boolean>;
