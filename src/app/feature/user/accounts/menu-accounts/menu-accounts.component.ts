3e8baf0bb3677dce1a78c19b6d19c0c8c8d5fe72
wert2all <wert2all@gmail.com>
2024-11-07 19:23:47 +0200

    feat: create account switcher

---
+++ src/app/feature/user/accounts/menu-accounts/menu-accounts.component.ts
@@ -0,0 +1,36 @@
+import { Component, input, output, signal } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  saxArrowLeftOutline,
+  saxArrowRightOutline,
+} from '@ng-icons/iconsax/outline';
+import { Unique } from '../../../../app.types';
+import { CurrentAccountComponent } from '../current-account/current-account.component';
+
+type Account = Unique & {
+  name: string;
+  firstLetter: string;
+  avatar?: string;
+};
+@Component({
+  selector: 'app-menu-accounts',
+  standalone: true,
+  templateUrl: './menu-accounts.component.html',
+  imports: [CurrentAccountComponent, NgIconComponent],
+  viewProviders: [provideIcons({ saxArrowLeftOutline, saxArrowRightOutline })],
+})
+export class MenuAccountsComponent {
+  currentAccount = input.required<Account | null>();
+  accounts = input<Account[]>([]);
+  isAccountChange = signal(false);
+  selectAccount = output<Unique>();
+
+  changeAccountClick() {
+    this.isAccountChange.set(!this.isAccountChange());
+  }
+
+  selectAccountClick(account: Account) {
+    this.selectAccount.emit(account);
+    this.changeAccountClick();
+  }
+}
