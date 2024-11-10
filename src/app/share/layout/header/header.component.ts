fdd5ecab9b9f27a5f5f1f7c972a2e20bab561cbf
Serhii Shvets <wert2all@gmail.com>
2024-11-10 15:51:44 +0200

    feat: turn on dropdown menu

--- src/app/share/layout/header/header.component.ts
+++ src/app/share/layout/header/header.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { saxGhostOutline, saxMenu1Outline } from '@ng-icons/iconsax/outline';
 import { Store } from '@ngrx/store';
 import { FeatureFlagComponent } from '../../../feature/flag/feature-flag/feature-flag.component';
 import { ThemeSwitchComponent } from '../../../feature/ui/theme/theme-switch.component';
@@ -11,28 +9,20 @@ import { TopMenuComponent } from '../../../feature/user/top-menu/top-menu.compon
 import { AuthStorageService } from '../../../store/auth/auth-storage.service';
 import { AuthActions } from '../../../store/auth/auth.actions';
 import { authFeature } from '../../../store/auth/auth.reducer';
-import { LegacyUserButtonComponent } from './legacy-user-button/legacy-user-button.component';
-import { CopyTokenComponent } from './top-menu/copy-token/copy-token.component';
-import { LoginButtonComponent } from './top-menu/login-button/login-button.component';
-import { LogoutButtonComponent } from './top-menu/logout-button/logout-button.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  viewProviders: [provideIcons({ saxMenu1Outline, saxGhostOutline })],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     TopMenuComponent,
-    NgIconComponent,
     LoginButtonComponent,
-    LogoutButtonComponent,
-    CopyTokenComponent,
     FeatureFlagComponent,
     ShowUserFeaturesComponent,
     ThemeSwitchComponent,
-    LegacyUserButtonComponent,
   ],
 })
 export class HeaderComponent {
