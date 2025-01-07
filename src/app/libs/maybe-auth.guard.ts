77c2157da71c78049a01132cba102c4b081b3a2a
Serhii Shvets <wert2all@gmail.com>
2025-01-07 22:26:28 +0200

    fix: fix guard of user dashboard

--- src/app/libs/maybe-auth.guard.ts
+++ src/app/libs/maybe-auth.guard.ts
@@ -1,20 +1,15 @@
 import { inject } from '@angular/core';
 import type { CanActivateFn } from '@angular/router';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
 import { AuthService } from '../services/auth.service';
-import { accountFeature } from '../store/account/account.reducer';
 
 export const maybeAuthGuard: CanActivateFn = () => {
-  const store = inject(Store);
   const router = inject(Router);
   const authService = inject(AuthService);
-  const isAuthorized = store.selectSignal(accountFeature.isAuthorized)();
   const isEmptyToken = !!authService.idToken;
-  // console.log(isAuthorized, isEmptyToken);
-  const canActivate = isAuthorized || isEmptyToken;
-  if (canActivate == false) {
+  // console.log('auth', isEmptyToken);
+  if (isEmptyToken == false) {
     router.navigateByUrl('/');
   }
-  return canActivate;
+  return true;
 };
