50e0d20d6e1e8d70643521af0c0903003cc0fcb2
wert2all <wert2all@gmail.com>
2025-01-10 16:03:32 +0200

    refactor: refactor add event button to float action button

--- src/app/feature/timeline/components/add-event-button/add-event-button.component.ts
+++ src/app/feature/timeline/components/add-event-button/add-event-button.component.ts
@@ -6,20 +6,20 @@ import {
   output,
 } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import {
-  saxAddOutline,
-  saxArrowRight1Outline,
-} from '@ng-icons/iconsax/outline';
+import { saxAddOutline, saxEditOutline } from '@ng-icons/iconsax/outline';
 
 @Component({
   selector: 'app-add-event-button',
   standalone: true,
   imports: [CommonModule, NgIconComponent],
   templateUrl: './add-event-button.component.html',
+  styleUrls: ['./add-event-button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  viewProviders: [provideIcons({ saxAddOutline, saxArrowRight1Outline })],
+  viewProviders: [provideIcons({ saxAddOutline, saxEditOutline })],
 })
 export class AddEventButtonComponent {
   disabled = input<boolean>(false);
+  withTooltip = input<boolean>(false);
+  showTooltip = input<boolean>(false);
   addEvent = output();
 }
