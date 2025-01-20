00741d57edfb9c15c085298a1d22fccfa86612c1
wert2all <wert2all@gmail.com>
2025-01-20 15:40:15 +0200

    refactor: refactor loading button

--- src/app/share/layout/content/loading-button/loading-button.component.ts
+++ src/app/share/layout/content/loading-button/loading-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { NgIconComponent } from '@ng-icons/core';
 
 @Component({
@@ -9,8 +9,14 @@ import { NgIconComponent } from '@ng-icons/core';
 })
 export class LoadingButtonComponent {
   isLoading = input.required<boolean>();
+
+  isDisabled = input<boolean>(false);
   icon = input<string | null>(null);
   style = input<string>('primary');
 
   clickEvent = output();
+
+  protected shouldDisable = computed(
+    () => this.isDisabled() || this.isLoading()
+  );
 }
