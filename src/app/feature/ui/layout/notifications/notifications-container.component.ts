cffaeb0a4abdae1789ff0fe06d03d871315cbceb
Serhii Shvets <wert2all@gmail.com>
2025-01-22 01:41:28 +0200

    refactor: move notifications

---
+++ src/app/feature/ui/layout/notifications/notifications-container.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import { NotificationMessage } from '../../../../store/notifications/notifications.types';
+
+@Component({
+  selector: 'app-notifications-container',
+  standalone: true,
+  imports: [CommonModule],
+  styleUrls: ['./notifications-container.component.scss'],
+  templateUrl: './notifications-container.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NotificationsContainerComponent {
+  messages = input.required<NotificationMessage[]>();
+  markRead = output<NotificationMessage>();
+}
