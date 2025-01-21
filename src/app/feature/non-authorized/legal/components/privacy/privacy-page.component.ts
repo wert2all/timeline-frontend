f17deaa770579b75fa3c5e5e629e4e0d33a3c239
Serhii Shvets <wert2all@gmail.com>
2025-01-21 23:18:30 +0200

    refactor: remove selectors from legal pages

--- src/app/feature/non-authorized/legal/components/privacy/privacy-page.component.ts
+++ src/app/feature/non-authorized/legal/components/privacy/privacy-page.component.ts
@@ -4,7 +4,6 @@ import { LayoutComponent } from '../../../../../shared/layout/layout.component';
 
 @Component({
   standalone: true,
-  selector: 'app-privacy-page',
   templateUrl: './privacy-page.component.html',
   styleUrls: ['./privacy-page.component.scss'],
   imports: [LayoutComponent, TitleComponent],
