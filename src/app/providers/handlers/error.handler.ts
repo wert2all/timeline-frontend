370327939c4bd8d4ffbf327835fc997538eaedf2
wert2all <wert2all@gmail.com>
2025-01-28 12:26:01 +0200

    fix: logout for invalid auth token

--- src/app/providers/handlers/error.handler.ts
+++ src/app/providers/handlers/error.handler.ts
@@ -1,14 +1,26 @@
+import { inject } from '@angular/core';
 import { ApolloLink } from '@apollo/client/core';
 import { onError } from '@apollo/client/link/error';
+import { Store } from '@ngrx/store';
+import { GraphQLFormattedErrorExtensions } from 'graphql/error/GraphQLError';
+import { SharedActions } from '../../shared/store/shared/shared.actions';
 
-export const handleError = (): ApolloLink => {
+export const handleError = (store = inject(Store)): ApolloLink => {
   return onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors)
-      graphQLErrors.map(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-        )
-      );
+      graphQLErrors.forEach(({ message, locations, path, extensions }) => {
+        if (checkAuthorizationError(extensions)) {
+          store.dispatch(SharedActions.logout());
+        } else {
+          return console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          );
+        }
+      });
     if (networkError) console.log(`[Network error]:`, networkError);
   });
 };
+
+const checkAuthorizationError = (
+  extension: GraphQLFormattedErrorExtensions | undefined
+) => (extension?.['code'] as string).startsWith('auth/');
