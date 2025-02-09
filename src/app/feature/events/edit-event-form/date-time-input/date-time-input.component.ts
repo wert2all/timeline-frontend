ada0766b7cca4b691e439cbf291d1eaa154ea6c8
wert2all <wert2all@gmail.com>
2025-02-09 16:59:06 +0200

    refactor: mode date picker to shared/ui namespace

--- src/app/feature/events/edit-event-form/date-time-input/date-time-input.component.ts
+++ src/app/feature/events/edit-event-form/date-time-input/date-time-input.component.ts
@@ -1,13 +1,13 @@
 import { Component, input } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DateTime } from 'luxon';
-import { DatePickerComponent } from '../../../authorized/dashboard/date-picker/date-picker.component';
+import { SharedDatePickerComponent } from '../../../../shared/ui/date-picker/date-picker.component';
 import { EditEventForm } from '../edit-event-form.types';
 
 @Component({
   selector: 'app-edit-event-form-date-time-input',
   templateUrl: './date-time-input.component.html',
-  imports: [DatePickerComponent, ReactiveFormsModule],
+  imports: [SharedDatePickerComponent, ReactiveFormsModule],
 })
 export class EditFormDateTimeInputComponent {
   form = input.required<FormGroup<EditEventForm>>();
