7db8e3ec83f51b287292b63c3e426f5ab8449447
wert2all <wert2all@gmail.com>
2025-02-11 12:24:20 +0200

    fix: enable production mode for app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+import { enableProdMode } from '@angular/core';
 import * as Sentry from '@sentry/angular';
 import { environment } from './environments/environment';
+import { EnvironmentType } from './environments/environment.types';
 
 Sentry.init({
   dsn: environment.services.sentry.dsn,
@@ -18,5 +20,9 @@ Sentry.init({
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
 });
+
+if (environment.type === EnvironmentType.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
