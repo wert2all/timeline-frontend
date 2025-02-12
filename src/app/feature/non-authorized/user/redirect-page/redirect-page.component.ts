2ecac74d03d525cb84b94502688dcefa1b6cd9b9
wert2all <wert2all@gmail.com>
2025-02-12 12:20:04 +0200

    fix: fix redirect page

--- src/app/feature/non-authorized/user/redirect-page/redirect-page.component.ts
+++ src/app/feature/non-authorized/user/redirect-page/redirect-page.component.ts
@@ -1,40 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  saxInformationOutline,
+  saxLoginOutline,
+} from '@ng-icons/iconsax/outline';
+import { Store } from '@ngrx/store';
+import { map, of } from 'rxjs';
+import { ApiClient } from '../../../../api/internal/graphql';
+import {
+  apiAssertNotNull,
+  extractApiData,
+} from '../../../../libs/api.functions';
 import { HeroComponent } from '../../../../shared/content/hero/hero.component';
 import { LayoutComponent } from '../../../../shared/layout/layout.component';
+import { SharedActions } from '../../../../shared/store/shared/shared.actions';
+import { NewAuthService } from '../../../auth/shared/auth.service';
 
 @Component({
   standalone: true,
   selector: 'app-redirect',
   templateUrl: './redirect-page.component.html',
-  imports: [LayoutComponent, HeroComponent],
+  imports: [LayoutComponent, HeroComponent, NgIconComponent],
+  viewProviders: [provideIcons({ saxInformationOutline, saxLoginOutline })],
 })
 export class LoginRedirectPageComponent {
-  // private readonly store = inject(Store);
-  // private readonly onAuth = inject(AuthFacade).onAuth;
-  //
-  // private readonly accountsProvider = inject(CachedAccountsProvider);
-  // private readonly currentAccountIdProvider = inject(CurrentAccountProvider);
-  //
-  // private readonly resource = rxResource({
-  //   request: this.onAuth,
-  //   loader: params => (params ? this.accountsProvider.getAccounts() : of(null)),
-  // });
+  private readonly authService = inject(NewAuthService);
+  private readonly api = inject(ApiClient);
+  private readonly store = inject(Store);
+
+  private readonly isAuthenticated = toSignal(
+    of(this.authService.hasValidToken())
+  );
+
+  private readonly apiAuth = rxResource({
+    request: this.isAuthenticated,
+    loader: ({ request }) => {
+      if (request) {
+        return this.api
+          .authorize()
+          .pipe(
+            map(result =>
+              apiAssertNotNull(
+                extractApiData(result)?.profile?.accounts,
+                'Could not authorize'
+              )
+            )
+          );
+      }
+      throw new Error('Not authenticated');
+    },
+  });
+
+  protected readonly error = computed(() => this.apiAuth.error());
+  protected readonly accounts = computed(() =>
+    this.apiAuth.value()?.filter(account => !!account)
+  );
 
   constructor() {
-    // effect(() => {
-    //   const accounts = this.resource.value();
-    //   if (accounts) {
-    //     const activeAccountId =
-    //       this.currentAccountIdProvider.getActiveAccountId();
-    //     const currentAccount =
-    //       accounts.find(a => a.id === activeAccountId) ||
-    //       accounts.slice(0, 1)[0];
-    //
-    //     this.currentAccountIdProvider.setActiveAccountId(currentAccount);
-    //     this.store.dispatch(
-    //       SharedActions.setActiveAccountAndRedirect({ account: currentAccount })
-    //     );
-    //   }
-    // });
+    effect(() => {
+      const accounts = this.accounts();
+      if (accounts) {
+        this.store.dispatch(SharedActions.successAuthenticated({ accounts }));
+      }
+    });
+  }
+
+  login() {
+    this.authService.login();
   }
 }
