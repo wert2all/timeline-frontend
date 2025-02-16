2fd52eef80cc79293e6e317ef300354ef7c68866
Serhii Shvets <wert2all@gmail.com>
2025-02-16 15:35:17 +0200

    fix: fix auth guard

--- src/app/feature/auth/shared/auth.guard.ts
+++ src/app/feature/auth/shared/auth.guard.ts
@@ -1,15 +1,23 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable, filter, switchMap, tap } from 'rxjs';
+import { concatLatestFrom } from '@ngrx/operators';
+import { Store } from '@ngrx/store';
+import { Observable, filter, map, switchMap, tap } from 'rxjs';
+import { accountFeature } from '../../account/store/account.reducer';
 import { NewAuthService } from './auth.service';
 
 export const canAuth = (
   _route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot,
-  authService = inject(NewAuthService)
+  authService = inject(NewAuthService),
+  store = inject(Store)
 ): Observable<boolean> =>
   authService.isDoneLoading$.pipe(
     filter(isDone => isDone),
     switchMap(() => authService.isAuthenticated$),
+    concatLatestFrom(() => store.select(accountFeature.selectActiveAccount)),
+    map(
+      ([isAuthenticated, activeAccount]) => isAuthenticated && !!activeAccount
+    ),
     tap(isAuthenticated => isAuthenticated || authService.login(state.url))
   );
