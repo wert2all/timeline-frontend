55c952161af2cae3546b2c0966c8852d5ef4298d
Serhii <wert2all@gmail.com>
2025-02-17 12:37:24 +0200

    feat: load account avatar after init application

--- src/app/feature/account/store/effects/images.effects.ts
+++ src/app/feature/account/store/effects/images.effects.ts
@@ -1,9 +1,12 @@
 import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
+import { Store } from '@ngrx/store';
 import { map } from 'rxjs';
 import { StoreDispatchEffect } from '../../../../app.types';
 import { SharedActions } from '../../../../shared/store/shared/shared.actions';
 import { AccountActions } from '../account.actions';
+import { accountFeature } from '../account.reducer';
 
 export const avatarEffects = {
   dispatchUpdateAvatarAfterUpload: createEffect((actions = inject(Actions)) => {
@@ -14,4 +17,25 @@ export const avatarEffects = {
       )
     );
   }, StoreDispatchEffect),
+
+  dispatchUpdateAccountAvatarsAfterInit: createEffect(
+    (actions = inject(Actions), store = inject(Store)) => {
+      return actions.pipe(
+        ofType(
+          AccountActions.setActiveAccountAfterInit,
+          AccountActions.setActiveAccountAfterAuth
+        ),
+        concatLatestFrom(() => store.select(accountFeature.selectAccounts)),
+        map(([_, accounts]) =>
+          SharedActions.dispatchLoadingImages({
+            ids: accounts
+              .map(account => account.avatar.id)
+              .filter(Boolean)
+              .map(id => id!),
+          })
+        )
+      );
+    },
+    StoreDispatchEffect
+  ),
 };
