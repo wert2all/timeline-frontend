1f2bf9724b5d4937460d34f2913c1ca191b1a53f
wert2all <wert2all@gmail.com>
2025-03-04 17:49:37 +0200

    fix: show error icon if could not load image

--- src/app/shared/ui/event/content/components/image/image.component.ts
+++ src/app/shared/ui/event/content/components/image/image.component.ts
@@ -1,4 +1,6 @@
 import { Component, computed, input } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { hugeImageDelete02 } from '@ng-icons/huge-icons';
 import { Status } from '../../../../../../app.types';
 import { SharedLoaderComponent } from '../../../../../content/loader/loader.component';
 import { EventContentImage } from '../../content.types';
@@ -7,12 +9,12 @@ import { EventContentImage } from '../../content.types';
   standalone: true,
   selector: 'app-timeline-event-image',
   templateUrl: './image.component.html',
-  imports: [SharedLoaderComponent],
+  imports: [SharedLoaderComponent, NgIconComponent],
+  viewProviders: [provideIcons({ hugeImageDelete02 })],
 })
 export class ImageComponent {
   image = input.required<EventContentImage>();
 
-  isLoading = computed(
-    () => this.image().status === Status.LOADING || !this.image().previewUrl
-  );
+  isLoading = computed(() => this.image().status === Status.LOADING);
+  isError = computed(() => this.image().status === Status.ERROR);
 }
