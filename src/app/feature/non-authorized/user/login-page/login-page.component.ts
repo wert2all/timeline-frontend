a178e80576e650030a747809f46ca31eee630e4b
Serhii Shvets <wert2all@gmail.com>
2025-04-06 21:24:51 +0300

    refactor: extract auth button to component

--- src/app/feature/non-authorized/user/login-page/login-page.component.ts
+++ src/app/feature/non-authorized/user/login-page/login-page.component.ts
@@ -1,30 +1,23 @@
-import { Component, inject, signal } from '@angular/core';
-import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { Component, inject } from '@angular/core';
+import { provideIcons } from '@ng-icons/core';
 import { phosphorSignIn } from '@ng-icons/phosphor-icons/regular';
 import { Store } from '@ngrx/store';
-import { OAuthService } from 'angular-oauth2-oidc';
 import { HeroComponent } from '../../../../shared/content/hero/hero.component';
 import { LayoutComponent } from '../../../../shared/layout/layout.component';
 import { sharedFeature } from '../../../../shared/store/shared/shared.reducers';
+import { SharedAuthButtonComponent } from '../../../account/share/auth-button/auth-button.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   templateUrl: './login-page.component.html',
   viewProviders: [provideIcons({ phosphorSignIn })],
-  imports: [LayoutComponent, NgIconComponent, HeroComponent],
+  imports: [LayoutComponent, HeroComponent, SharedAuthButtonComponent],
 })
 export class LoginPageComponent {
   private readonly store = inject(Store);
-  private readonly authService = inject(OAuthService);
 
-  protected isLoading = signal(false);
   protected canUseCookies = this.store.selectSignal(
     sharedFeature.canUseNecessaryCookies
   );
-
-  login() {
-    this.isLoading.set(true);
-    this.authService.initLoginFlow();
-  }
 }
