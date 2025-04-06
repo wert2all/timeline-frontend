a94089ee08b0a946e5630058551ca83a9a11d9c9
wert2all <wert2all@gmail.com>
2025-04-06 14:05:10 +0300

    feat: add menu action for copy event url

--- src/app/feature/timeline/components/event/menu/menu.component.ts
+++ src/app/feature/timeline/components/event/menu/menu.component.ts
@@ -1,6 +1,8 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { phosphorHeartDuotone } from '@ng-icons/phosphor-icons/duotone';
 import {
+  phosphorLinkSimpleHorizontal,
   phosphorPencilLine,
   phosphorTrash,
 } from '@ng-icons/phosphor-icons/regular';
@@ -11,11 +13,36 @@ import { Iterable } from '../../../../../app.types';
   imports: [NgIconComponent],
   selector: 'app-timeline-event-menu',
   templateUrl: './menu.component.html',
-  viewProviders: [provideIcons({ phosphorPencilLine, phosphorTrash })],
+  styleUrls: ['./menu.component.scss'],
+  viewProviders: [
+    provideIcons({
+      phosphorPencilLine,
+      phosphorTrash,
+      phosphorLinkSimpleHorizontal,
+      phosphorHeartDuotone,
+    }),
+  ],
 })
 export class TimelineEventMenuComponent {
   timelineEvent = input.required<Iterable>();
 
   delete = output<Iterable>();
   edit = output<Iterable>();
+  link = output<Iterable>();
+  like = output<Iterable>();
+
+  protected copied = signal(false);
+  protected liked = signal(false);
+
+  protected likeClick(item: Iterable) {
+    this.liked.set(true);
+    this.like.emit(item);
+    setTimeout(() => this.liked.set(false), 3000);
+  }
+
+  protected copyClick(item: Iterable) {
+    this.copied.set(true);
+    this.link.emit(item);
+    setTimeout(() => this.copied.set(false), 3000);
+  }
 }
