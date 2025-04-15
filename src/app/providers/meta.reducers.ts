4c6d813f406433a1edbcefd352a3a7d00c5a4a5f
wert2all <wert2all@gmail.com>
2025-04-15 14:54:44 +0300

    fix: fix auth

--- src/app/providers/meta.reducers.ts
+++ src/app/providers/meta.reducers.ts
@@ -1,15 +1,17 @@
 import { ActionReducer } from '@ngrx/store';
 import { localStorageSync } from 'ngrx-store-localstorage';
 import { accountFeature } from '../feature/account/store/account.reducer';
+import { UserAccountsKey } from '../feature/account/store/account.types';
 import { imagesFeature } from '../shared/store/images/images.reducer';
+import { ImagesKey } from '../shared/store/images/images.types';
 
 export const metaReducers = [
   /* eslint-disable  @typescript-eslint/no-explicit-any */
   (reducer: ActionReducer<any, any>) =>
     localStorageSync({
       keys: [
-        { [imagesFeature.name]: ['images'] },
-        { [accountFeature.name]: ['accounts'] },
+        { [imagesFeature.name]: [ImagesKey] },
+        { [accountFeature.name]: [UserAccountsKey] },
       ],
       rehydrate: true,
     })(reducer),
